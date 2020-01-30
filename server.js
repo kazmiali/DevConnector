d6ec186dd366ad4121e8cc1e472ba8899bc485b5
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2020-01-31 02:39:46 +0500

    :zap: Change port to 5000

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const enforce = require("express-sslify");
-const compression = require("compression");
-const path = require("path");
+const express = require('express');
+const connectDB = require('./config/db');
+const enforce = require('express-sslify');
+const compression = require('compression');
+const path = require('path');
 
 const app = express();
 
@@ -14,20 +14,20 @@ app.use(express.json({ extended: true }));
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/auth", require("./routes/api/auth"));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //  Set static folder
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+    //  Set static folder
+    app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}`));
